6c97984547b2cd97740de3569b996f42545528fc
Sean Hinchee <henesy.dev@gmail.com>
2015-02-01 22:56:17 -0600

    Added more set-up dialogue, cleaned up some code, added some comments

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ func main() {
 
 var (
 	usrrtn string
-	randz int
 	usrinpt string
 )
 
@@ -30,14 +29,27 @@ tf := false
 	fmt.Println("Version: ", version)
 	fmt.Println("Written and Built for your Pleasure by PND Tech Club")
 	fmt.Println("Enjoyment is Optional.")
-	usrrtn, tf = svi.YorN("Select Your Metatype?") // no [y/n]: is needed, as it is added
-	randz = svi.Random(1, 6) // gen a number 1-5
 	
+	// Basic Yes or No Dialogue, see `svi.go` for svi.*  functions
+	for {
+		usrrtn, tf = svi.YorN("Select Your Metatype?") // no [y/n]: is needed, as it is added
+		if tf == true {
+			fmt.Println("Okay.")
+			break
+		} else {
+			fmt.Println("Pick - yes or no - not that hard")
+		}
+	}
+
+	//randz := svi.Random(1, 6) // gen a number 1-5 (arbitrary)
+	
+	// Ask for metatype, then read from the relevant .meta file
 	if usrrtn == "y" {
 		for {
 			fmt.Print("Metatype [? for list]: ")
 			fmt.Scan(&usrinpt)
 			if usrinpt == "?" {
+				// There MUST be a better way to do this ... then again ...
 				fmt.Printf("\nInstigator - troublemaking bastards\nPreppy - \nStudent Ambassador - \nUnifier - \n")
 			}
 			metaname := usrinpt + ".meta"
@@ -54,6 +66,8 @@ tf := false
 		}
 	}
 
-	fmt.Println(usrrtn, tf, randz) // testing
-}
+
+
+	fmt.Println("Thanks for Playing :) ")
+} // Program Ends
 
